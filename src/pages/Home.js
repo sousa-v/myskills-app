43b0f4abf2b5aa8ff88d7a406864dda0f8f3b6bf
Victor <victorbrito001@gmail.com>
2022-04-25 14:37:15 -0300

    Propriedades no React Native

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,12 +23,12 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button />
+      <Button onPress={handleAddNewSkill} />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My skills</Text>
 
       {mySkills.map((skill) => (
-        <SkillCard />
+        <SkillCard skill={skill} />
       ))}
     </View>
   );
