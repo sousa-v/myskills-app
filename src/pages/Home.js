6e2298028bc44455afedbea3bc153e04ea53a8c8
Victor <victorbrito001@gmail.com>
2022-04-28 14:58:23 -0300

    Continuando com os Hooks useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -14,14 +14,28 @@ import { SkillCard } from "../components/SkillCard";
 export function Home() {
   const [newSkill, setNewSkill] = useState("");
   const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState([]);
 
   function handleAddNewSkill() {
     setMySkills((oldState) => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting("Good morning!");
+    } else if (currentHour >= 12 && currentHour <= 18) {
+      setGreeting("Good afternoon!");
+    } else {
+      setGreeting("Good night!");
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}> Welcome, Victor</Text>
+      <Text style={styles.greetings}> {greeting} </Text>
 
       <TextInput
         style={styles.input}
@@ -52,16 +66,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
   },
   title: {
-    color: "#fff",
+    color: "#FFF",
     fontSize: 24,
     fontWeight: "bold",
   },
   input: {
     backgroundColor: "#1F1E25",
-    color: "#fff",
+    color: "#FFF",
     fontSize: 18,
     padding: Platform.OS === "ios" ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: "#FFF",
   },
 });
