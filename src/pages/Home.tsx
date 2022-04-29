7286efd80279422d806040c7d40956e4ec858db5
Victor <victorbrito001@gmail.com>
2022-04-29 09:43:40 -0300

    Finalizando o App

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,6 +30,10 @@ export function Home() {
     setMySkills((oldState) => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
+  }
+
   useEffect(() => {
     const currentHour = new Date().getHours();
 
@@ -61,7 +65,12 @@ export function Home() {
       <FlatList
         data={mySkills}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <SkillCard skill={item.name} />}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
       />
     </View>
   );
@@ -71,7 +80,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#121015",
-    paddingHorizontal: 20,
     paddingVertical: 70,
     paddingHorizontal: 30,
   },
